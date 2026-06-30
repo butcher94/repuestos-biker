ca8f33a14c9b6875f8946d13ad777ed677719070
Nombre del estudiante <correo@ejemplo.com>
2026-06-29 20:35:59 -0600

    Conectar backend con MySQL Railway

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const conexion = mysql.createConnection({
 
     host: process.env.DB_HOST,
 
+    port: process.env.DB_PORT || 3306,
+
     user: process.env.DB_USER,
 
     password: process.env.DB_PASSWORD,
